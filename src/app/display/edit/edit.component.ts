36cc3283c481c8ff35e551b3b580c7076188eab8
snoopyrocks <l.letutour@live.fr>
2019-11-12 12:29:11 +0100

    display with sidebar

--- src/app/display/edit/edit.component.ts
+++ src/app/display/edit/edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
@@ -9,9 +9,30 @@ import {FormArray, FormBuilder, FormGroup} from '@angular/forms';
 export class EditComponent implements OnInit {
   faPlus = faPlus;
 
-  constructor() {
-  }
+  displayForm: FormGroup;
+  items: FormArray;
+
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.displayForm = this.formBuilder.group({
+      mainTitle: 'Ma premère doc',
+      items: this.formBuilder.array([this.createItem('textarea')])
+    });
+  }
+
+  createItem(type): FormGroup {
+    return this.formBuilder.group({
+      type: FormControl,
+      content: FormControl
+    });
+  }
+
+  addItem(type): void {
+    this.items = this.displayForm.get('items') as FormArray;
+    this.items.push(this.createItem(type));
+  }
+  onSubmit() {
+    console.warn(this.displayForm.value);
   }
 }
