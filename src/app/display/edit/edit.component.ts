5c248294c74790f8124e39f3995e451595f9c730
Lory Le Tutour <lory@cliclic.tv>
2019-12-04 10:21:15 +0100

    add content work

--- src/app/display/edit/edit.component.ts
+++ src/app/display/edit/edit.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import {FormArray, FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {DisplayService} from '../../services/display.service';
 import {DisplayModel} from '../../models/display/Display.model';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-edit',
@@ -12,13 +13,31 @@ export class EditComponent implements OnInit {
   faPlus = faPlus;
   displayForm: FormGroup;
   body: FormArray;
+  contents : Array<any>;
+
 
   constructor(
     private fb: FormBuilder,
     private displayService: DisplayService,
+    private router:Router
   ) {}
 
   ngOnInit() {
+    this.contents = [
+      {
+        content: 'dsfsf',
+        type: 'text',
+        id: 0,
+        index: 0
+      },
+      {
+        content: 5,
+        type: 'number',
+        id: 1,
+        index: 1
+      }
+    ];
+    console.log(this.contents);
     this.initForm();
   }
 
@@ -26,22 +45,44 @@ export class EditComponent implements OnInit {
     this.displayForm = this.fb.group({
       title: 'ee',
       body: this.fb.array(
-        [
-          this.fb.control({type: 'text', content: 'dsfsf', id: 0}),
-          this.fb.control({type: 'number', content: 5, id: 1}),
-        ]
-      )
+        this.contents.map(elem => this.addContents(elem)))
+    });
+  }
+
+  addContents(control) : FormGroup {
+    return this.fb.group({
+      content: this.fb.control([control.content]),
+      type: [control.type,[Validators.required]],
+      id: [control.id,[Validators.required]],
+      index: [control.index,[Validators.required]]
     });
   }
 
 
-  addField(type): void {
-    const add = new FormControl({content: '', type});
+  addField(type): FormGroup {
+    const add = this.fb.group({
+      content: type === 'code' ? 'code' : (type === 'title' ? 'Bienvenue' : 3),
+      type,
+      // @ts-ignore
+      id: this.displayForm.controls.body.controls.length,
+      // @ts-ignore
+      index: this.displayForm.controls.body.controls.length
+    });
     this.body = this.displayForm.get('body') as FormArray;
     this.body.push(add);
+
+    return;
   }
 
-    onSubmit() {
+  onSubmit() {
     console.log(this.displayForm.value);
-    }
+    const formValue = this.displayForm.value;
+    const entry = new DisplayModel(
+      formValue['title'],
+      formValue['title'],
+      formValue['body']
+    );
+    this.displayService.addContent(entry);
+    this.router.navigate(['/display']);
   }
+}
