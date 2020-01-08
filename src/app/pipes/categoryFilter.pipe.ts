23621f09f72755e25775fdf5534931fc08cdf58a
Mehdi <mdzouaoui@orange.fr>
2020-01-08 19:02:38 +0100

    WIP

--- src/app/pipes/categoryFilter.pipe.ts
+++ src/app/pipes/categoryFilter.pipe.ts
@@ -8,17 +8,21 @@ import {SnippetService} from "../services/snippet.service";
 })
 export class CategoryFilter implements PipeTransform {
 
-
-  transform(snippetsArray: Map<string, SnippetsModel> , categoryKey : any): SnippetsModel{
-    if (snippetsArray ) {
-      for( let id of snippetsArray.keys()){
-        if(categoryKey === snippetsArray.get(id).categoryId){
-          console.log(snippetsArray.get(id));
-          return snippetsArray.get(id);
-        }
-      }
-
-    }
+  transform(snippetsMap: Map<string, SnippetsModel> , categoryKey : any)  {
+    // return Array.from(snippetsMap.values()).filter(snippet => snippet.categoryId == categoryKey);
   }
-}
+
+//   transform(snippetsArray: Map<string, SnippetsModel> , categoryKey : any): SnippetsModel | undefined {
+//     if (snippetsArray ) {
+//       for( let id of snippetsArray.keys()){
+//         if(categoryKey === snippetsArray.get(id).categoryId){
+//           console.log(snippetsArray.get(id));
+//           return snippetsArray.get(id);
+//         }
+//       }
+//
+//     }
+//     return null ;
+  }
+
 
